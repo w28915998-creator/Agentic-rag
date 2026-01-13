2cc04e21503dd2248b8a9b2cade5f1039bfd57cf
w28915998-creator <w28915998@gmail.com>
2026-01-13 09:56:01 +0500

    Delete RAG new directory

--- RAG new/src/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Source package for the Agentic RAG system.
-"""
--- RAG new/src/db/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Database clients package.
-"""
-
-from .qdrant_client import QdrantManager
-from .neo4j_client import Neo4jManager
-
-__all__ = ["QdrantManager", "Neo4jManager"]
